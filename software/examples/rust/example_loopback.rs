0e45483bdccd13777472142d879ba07ac230555f
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:19:25 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_loopback.rs
+++ software/examples/rust/example_loopback.rs
@@ -1,5 +1,5 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{can_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{can_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 4223;
@@ -7,24 +7,24 @@ const UID: &str = "XYZ"; // Change XYZ to the UID of your CAN Bricklet
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection
-    let can_bricklet = CANBricklet::new(UID, &ipcon); // Create device object
+    let can = CanBricklet::new(UID, &ipcon); // Create device object
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
+                                          // Don't use device before ipcon is connected
 
     // Configure transceiver for loopback mode
-    can_bricklet.set_configuration(CAN_BRICKLET_BAUD_RATE_1000KBPS, CAN_BRICKLET_TRANSCEIVER_MODE_LOOPBACK, 0);
+    can.set_configuration(CAN_BRICKLET_BAUD_RATE_1000KBPS, CAN_BRICKLET_TRANSCEIVER_MODE_LOOPBACK, 0);
 
-    //Create listener for frame read events.
-    let frame_read_listener = can_bricklet.get_frame_read_receiver();
-    // Spawn thread to handle received events. This thread ends when the can_bricklet
+    //Create receiver for frame read events.
+    let frame_read_receiver = can.get_frame_read_receiver();
+    // Spawn thread to handle received events. This thread ends when the can
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in frame_read_listener {
-            println!("Frame Type: {}", event.frame_type);
-            println!("Identifier: {}", event.identifier);
-            print!("Data (Length: {}):", event.length);
-            for item in event.data.iter() {
+        for frame_read in frame_read_receiver {
+            println!("Frame Type: {}", frame_read.frame_type);
+            println!("Identifier: {}", frame_read.identifier);
+            print!("Data (Length: {}):", frame_read.length);
+            for item in frame_read.data.iter() {
                 print!(" {}", item);
             }
             println!();
@@ -33,15 +33,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Enable frame read callback
-    can_bricklet.enable_frame_read_callback();
+    can.enable_frame_read_callback();
 
     let data = [42u8, 23, 17, 0, 0, 0, 0, 0];
-    can_bricklet.write_frame(CAN_BRICKLET_FRAME_TYPE_STANDARD_DATA, 1742, data, 3);
+    can.write_frame(CAN_BRICKLET_FRAME_TYPE_STANDARD_DATA, 1742, data, 3);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    can_bricklet.disable_frame_read_callback();
+    can.disable_frame_read_callback();
     ipcon.disconnect();
     Ok(())
 }
